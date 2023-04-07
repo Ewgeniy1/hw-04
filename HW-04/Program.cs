b90a8db77ed355492a4b2e3c276800d5d9b9d92c
Bismarc <Bismarc@BISMARC>
2023-04-07 21:02:56 +0300

    Добавьте файлы проекта.

---
+++ HW-04/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Globalization;
+
+namespace hw_04
+{
+    internal class Arraay
+    {
+        static void Main(string[] args)
+        {
+
+            int temp;
+            Random rand = new Random();
+            int[] Array = new int[7];
+            Console.WriteLine("Random array");
+            for (int i = 0; i < Array.Length; i++)
+            {
+                Array[i] = rand.Next(-20, 20);
+                Console.WriteLine(Array[i]);
+            }
+            Console.WriteLine();
+            int counter1 = 0;
+            for (int i = 1; i < Array.Length; i++)
+            {
+                for (int j = 0; j < Array.Length - 1; j++)
+                {
+                    if (Array[j] > Array[j])
+                    {
+                        temp = Array[j + 1];
+                        Array[j + 1] = Array[j];
+                        Array[j] = temp;
+                        counter1++;
+                    }
+                }
+            }
+            Console.WriteLine("Sorted arrays in  order");
+
+
+
+
+
+
+
+
+
+            for (int i = 0; i < Array.Length; i++)
+            {
+                Console.WriteLine(Array[i]);
+
+            }
+            Console.WriteLine($"Sorting of this array took{counter1}iterations");
+            int t = Array.Length;
+            int k = t / 2;
+            int temp2;
+            int counter2 = 0;
+            for (int i = 0; i < k; i++)
+            {
+                temp2 = Array[i];
+                Array[i] = Array[t - i - 1];
+                Array[t - i - 1] = temp2;
+                counter2++;
+            }
+            Console.WriteLine("Sorted array in descending order: ");
+            foreach (int i in Array)
+            {
+                Console.WriteLine($"{i}");
+            }
+            Console.WriteLine("Sorting of this array took{counter2}iterations");
+
+
+
+        }
+    }
+}
---
+++ HW-04/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("HW-04")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("HW-04")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("97a0387b-b2b6-4c5d-af1c-2d9a242a2e3d")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
